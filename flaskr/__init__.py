aabe6d6049e1f985f786e27ff3143fb324de9079
zrr12138 <1520407273@qq.com>
2022-06-03 19:44:25 +0800

    博客蓝图

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,6 +27,9 @@ def create_app(test_config=None):
     db.init_app(app)
     from . import auth
     app.register_blueprint(auth.bp)
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     # a simple page that says hello
     @app.route('/hello')
     def hello():
